67355867c32c490b4835f45f553ef2c09e7ea222
zahran <ahmad2003zahran@gmail.com>
2026-06-27 02:52:38 -0700

    fix: DbContext in root namespace

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -2,7 +2,7 @@ using CyberShield.API.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace CyberShield.API.Data
+namespace CyberShield.API
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
@@ -27,55 +27,30 @@ namespace CyberShield.API.Data
 
             builder.Entity<UserSubscription>(entity =>
             {
-                entity.HasOne(s => s.User)
-                    .WithMany()
-                    .HasForeignKey(s => s.UserId)
-                    .OnDelete(DeleteBehavior.Restrict);
-
-                entity.HasOne(s => s.Package)
-                    .WithMany(p => p.Subscriptions)
-                    .HasForeignKey(s => s.PackageId)
-                    .OnDelete(DeleteBehavior.Restrict);
+                entity.HasOne(s => s.User).WithMany().HasForeignKey(s => s.UserId).OnDelete(DeleteBehavior.Restrict);
+                entity.HasOne(s => s.Package).WithMany(p => p.Subscriptions).HasForeignKey(s => s.PackageId).OnDelete(DeleteBehavior.Restrict);
             });
 
             builder.Entity<PackageFeature>(entity =>
             {
-                entity.HasOne(pf => pf.Package)
-                    .WithMany(p => p.PackageFeatures)
-                    .HasForeignKey(pf => pf.PackageId)
-                    .OnDelete(DeleteBehavior.Cascade);
-
-                entity.HasOne(pf => pf.Feature)
-                    .WithMany(f => f.PackageFeatures)
-                    .HasForeignKey(pf => pf.FeatureId)
-                    .OnDelete(DeleteBehavior.Restrict);
+                entity.HasOne(pf => pf.Package).WithMany(p => p.PackageFeatures).HasForeignKey(pf => pf.PackageId).OnDelete(DeleteBehavior.Cascade);
+                entity.HasOne(pf => pf.Feature).WithMany(f => f.PackageFeatures).HasForeignKey(pf => pf.FeatureId).OnDelete(DeleteBehavior.Restrict);
             });
 
             builder.Entity<ApplicationUser>(entity =>
             {
-                entity.HasOne(u => u.CurrentPackage)
-                    .WithMany()
-                    .HasForeignKey(u => u.CurrentPackageId)
-                    .OnDelete(DeleteBehavior.SetNull);
+                entity.HasOne(u => u.CurrentPackage).WithMany().HasForeignKey(u => u.CurrentPackageId).OnDelete(DeleteBehavior.SetNull);
             });
 
             builder.Entity<FeatureUsageHistory>(entity =>
             {
-                entity.HasOne(h => h.Feature)
-                    .WithMany()
-                    .HasForeignKey(h => h.FeatureId)
-                    .OnDelete(DeleteBehavior.Restrict);
+                entity.HasOne(h => h.Feature).WithMany().HasForeignKey(h => h.FeatureId).OnDelete(DeleteBehavior.Restrict);
             });
 
             builder.Entity<FeatureUsageCounter>(entity =>
             {
-                entity.HasOne(c => c.Feature)
-                    .WithMany()
-                    .HasForeignKey(c => c.FeatureId)
-                    .OnDelete(DeleteBehavior.Restrict);
-
-                entity.HasIndex(c => new { c.UserId, c.FeatureId, c.Year, c.Month })
-                    .IsUnique();
+                entity.HasOne(c => c.Feature).WithMany().HasForeignKey(c => c.FeatureId).OnDelete(DeleteBehavior.Restrict);
+                entity.HasIndex(c => new { c.UserId, c.FeatureId, c.Year, c.Month }).IsUnique();
             });
 
             builder.Entity<AdminAuditLog>(entity =>
